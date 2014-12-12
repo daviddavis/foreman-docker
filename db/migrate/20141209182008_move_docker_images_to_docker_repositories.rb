ba0f080a830df15c710d3eb4cf3a4432bd549252
David Davis <daviddavis@redhat.com>
2014-12-12 15:29:14 -0500

    Refs #8632 - Fixing the FK calls in the migration

--- db/migrate/20141209182008_move_docker_images_to_docker_repositories.rb
+++ db/migrate/20141209182008_move_docker_images_to_docker_repositories.rb
@@ -1,12 +1,15 @@
 class MoveDockerImagesToDockerRepositories < ActiveRecord::Migration
   def change
-    rename_index :docker_tags, :docker_tags_docker_image_id_fk, :docker_tags_docker_repository_id_fk
-    rename_index :containers, :containers_docker_image_id_fk, :containers_docker_repository_id_fk
+    remove_foreign_key :docker_tags, :name => :docker_tags_docker_image_id_fk
+    remove_foreign_key :containers, :name => :containers_docker_image_id_fk
 
     rename_table :docker_images, :docker_repositories
 
     rename_column :docker_repositories, :image_id, :name
     rename_column :containers,  :docker_image_id, :docker_repository_id
     rename_column :docker_tags, :docker_image_id, :docker_repository_id
+
+    add_foreign_key :docker_tags, :docker_repositories
+    add_foreign_key :containers, :docekr_repositories
   end
 end
